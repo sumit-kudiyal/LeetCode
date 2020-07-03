a28710941ed19fa9d1d89494ea3ab459679c6d65
sumit-kudiyal <39873769+sumit-kudiyal@users.noreply.github.com>
2020-07-03 23:30:42 +0530

    Update Bellman_Ford's_ Algorithm.cpp

--- Algorithm/Graph/Bellman_Ford's_ Algorithm.cpp
+++ Algorithm/Graph/Bellman_Ford's_ Algorithm.cpp
@@ -1,1 +1,41 @@
+/*
+Bellman Ford's algorithm is used to find the shortest paths from the source vertex to all other vertices in a weighted graph. It depends on the following concept: Shortest path contains at most  edges, because the shortest path couldn't have a cycle.
 
+So why shortest path shouldn't have a cycle ?
+There is no need to pass a vertex again, because the shortest path to all other vertices could be found without the need for a second visit for any vertices.
+
+Algorithm Steps:
+
+The outer loop traverses from  : .
+Loop over all edges, check if the next node distance > current node distance + edge weight, in this case update the next node distance to "current node distance + edge weight".
+This algorithm depends on the relaxation principle where the shortest distance for all vertices is gradually replaced by more accurate values until eventually reaching the optimum solution. In the beginning all vertices have a distance of "Infinity", but only the distance of the source vertex = , then update all the connected vertices with the new distances (source vertex distance + edge weights), then apply the same concept for the new vertices with new distances and so on.
+*/
+
+
+vector<int> v[2000 +10];
+int dis[1000 +10]
+
+for(int i=0;i<m+2;i++){
+  v[i].clear();
+  dis[i]= INT_MAX;
+}
+
+for(int i = 0; i < m; i++){
+
+        scanf("%d%d%d", &from , &next , &weight);
+        v[i].push_back(from);
+        v[i].push_back(next);
+        v[i].push_back(weight);
+   }
+
+    dis[0] = 0;
+    for(int i = 0; i < n - 1; i++){
+        int j = 0;
+        while(v[j].size() != 0){
+
+            if(dis[ v[j][0]  ] + v[j][2] < dis[ v[j][1] ] ){
+                dis[ v[j][1] ] = dis[ v[j][0]  ] + v[j][2];
+            }
+            j++;
+        }
+    }
